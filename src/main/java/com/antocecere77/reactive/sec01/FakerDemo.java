9d258b6cc967d4695dd7bb930f731630f29d7f83
antocecere <antocecere77@gmail.com>
2021-01-29 06:48:48 +0100

    Use of faker

---
+++ src/main/java/com/antocecere77/reactive/sec01/FakerDemo.java
@@ -0,0 +1,13 @@
+package com.antocecere77.reactive.sec01;
+
+import com.github.javafaker.Faker;
+
+public class FakerDemo {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<10; i++) {
+            System.out.println(Faker.instance().name().fullName());
+        }
+    }
+}
