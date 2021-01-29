0a3986c295d27cfc3fb9b42638a737df31633652
antocecere <antocecere77@gmail.com>
2021-01-29 07:26:13 +0100

    [Mono] - block

--- src/main/java/com/antocecere77/reactive/sec01/Lec06SupplierRefactoring.java
+++ src/main/java/com/antocecere77/reactive/sec01/Lec06SupplierRefactoring.java
@@ -8,9 +8,11 @@ public class Lec06SupplierRefactoring {
 
     public static void main(String[] args) {
         getName();
-        getName()
+        String name = getName()
                 .subscribeOn(Schedulers.boundedElastic())
-                .subscribe(Util.onNext());
+                //subscribe(Util.onNext());
+                .block();
+        System.out.println(name);
         getName();
 
         Util.sleepSeconds(4);
