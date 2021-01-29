571cb0c4e681e1363c52351cc3eaa0547042b58a
antocecere <antocecere77@gmail.com>
2021-01-29 06:57:56 +0100

    [Mono] - emitting empty / error

---
+++ src/main/java/com/antocecere77/reactive/sec01/Lec04MonoEmptyOrError.java
@@ -0,0 +1,27 @@
+package com.antocecere77.reactive.sec01;
+
+import com.antocecere77.reactive.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec04MonoEmptyOrError {
+
+    public static void main(String[] args) {
+        userRepository(3)
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        Util.onComplete()
+                );
+    }
+
+    private static Mono<String> userRepository(int userId) {
+        // 1
+        if(userId==1) {
+            return Mono.just(Util.faker().name().firstName());
+        } else if(userId==2) {
+            return Mono.empty();
+        } else {
+            return Mono.error(new RuntimeException("Not in the allowed range"));
+        }
+    }
+}
