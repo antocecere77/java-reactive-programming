1f09466b4c0fc01469a22731f70ef9d21182a360
antocecere <antocecere77@gmail.com>
2021-01-29 06:38:05 +0100

    [Mono] - subscribe - onerror

--- src/main/java/com/antocecere77/reactive/sec01/Lec03MonoSubscribe.java
+++ src/main/java/com/antocecere77/reactive/sec01/Lec03MonoSubscribe.java
@@ -7,9 +7,22 @@ public class Lec03MonoSubscribe {
     public static void main(String[] args) {
 
         //pusblisher
-        Mono<String> mono = Mono.just("ball");
+        //Mono<String> mono = Mono.just("ball");
 
-        mono.subscribe(
+//        mono.subscribe(
+//                item -> System.out.println(item),
+//                err -> System.out.println(err.getMessage()),
+//                () -> System.out.println("Completed")
+//        );
+
+        //pusblisher
+        final var monoInteger = Mono.just("ball")
+                .map(String::length)
+                .map(l -> l / 0);
+
+        monoInteger.subscribe();
+
+        monoInteger.subscribe(
                 item -> System.out.println(item),
                 err -> System.out.println(err.getMessage()),
                 () -> System.out.println("Completed")
