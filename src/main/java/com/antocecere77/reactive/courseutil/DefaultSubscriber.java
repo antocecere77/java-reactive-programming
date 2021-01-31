4c0fee5e20013f9232842b4643371413d4336306
antocecere <antocecere77@gmail.com>
2021-01-31 10:35:15 +0100

    Default subscriber implementation

---
+++ src/main/java/com/antocecere77/reactive/courseutil/DefaultSubscriber.java
@@ -0,0 +1,36 @@
+package com.antocecere77.reactive.courseutil;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+@NoArgsConstructor
+public class DefaultSubscriber implements Subscriber<Object> {
+
+    private String name;
+
+    public DefaultSubscriber(String name) {
+        this.name = name + " - ";
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        subscription.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onNext(Object o) {
+        System.out.println(name + "received: " + o);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.out.println(name + "ERROR " + throwable.getStackTrace());
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println(name + "completed!");
+    }
+}
