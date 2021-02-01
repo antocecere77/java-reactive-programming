48d834bf622b547d396ac1d3b2eaf53f24509a25
antocecere <antocecere77@gmail.com>
2021-02-01 07:24:03 +0100

    Operator - switch on first

---
+++ src/main/java/com/antocecere77/reactive/sec04/Lec11SwitchOnFirst.java
@@ -0,0 +1,34 @@
+package com.antocecere77.reactive.sec04;
+
+import com.antocecere77.reactive.courseutil.Util;
+import com.antocecere77.reactive.sec04.helper.Person;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+public class Lec11SwitchOnFirst {
+
+    public static void main(String[] args) {
+
+        getPerson()
+                .switchOnFirst(((signal, personFlux) -> {
+                    System.out.println("inside switch-on-first");
+                    return signal.isOnNext() && signal.get().getAge() > 10
+                            ? personFlux
+                            : applyFilterMap().apply(personFlux);
+                }))
+                .subscribe(Util.subscriber());
+    }
+
+    public static Flux<Person> getPerson() {
+        return Flux.range(1, 10)
+                .map(i -> new Person());
+    }
+
+    public static Function<Flux<Person>, Flux<Person>> applyFilterMap() {
+        return flux -> flux
+                .filter(p -> p.getAge() > 10)
+                .doOnNext(p -> p.setName(p.getName().toUpperCase()))
+                .doOnDiscard(Person.class, p -> System.out.println("Not allowing: " + p));
+    }
+}
