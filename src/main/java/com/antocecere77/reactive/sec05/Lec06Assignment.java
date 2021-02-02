4427538587c9ba565d735bd3fca4acb4b43cf942
antocecere <antocecere77@gmail.com>
2021-02-02 07:41:56 +0100

    Inventory/revenue stream

---
+++ src/main/java/com/antocecere77/reactive/sec05/Lec06Assignment.java
@@ -0,0 +1,28 @@
+package com.antocecere77.reactive.sec05;
+
+import com.antocecere77.reactive.courseutil.Util;
+import com.antocecere77.reactive.sec05.assignment.InventoryService;
+import com.antocecere77.reactive.sec05.assignment.OrderService;
+import com.antocecere77.reactive.sec05.assignment.RevenueService;
+
+public class Lec06Assignment {
+
+    public static void main(String[] args) {
+
+        OrderService orderService = new OrderService();
+        RevenueService revenueService = new RevenueService();
+        InventoryService inventoryService = new InventoryService();
+
+        orderService.orderStream().subscribe(revenueService.subscribeOrderStream());
+        orderService.orderStream().subscribe(inventoryService.subscribeOrderStream());
+
+        inventoryService.inventoryStream()
+                .subscribe(Util.subscriber("inventory"));
+
+        revenueService.revenueStream()
+                .subscribe(Util.subscriber("revenue"));
+
+        Util.sleepSeconds(60);
+
+    }
+}
