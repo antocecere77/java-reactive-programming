3a1dd0229a104a276083b40ff94e30594faafb0f
antocecere <antocecere77@gmail.com>
2021-02-04 06:50:54 +0100

    Overflow strategy - drop - part 2

--- src/main/java/com/antocecere77/reactive/sec07/Lec02Drop.java
+++ src/main/java/com/antocecere77/reactive/sec07/Lec02Drop.java
@@ -3,15 +3,19 @@ package com.antocecere77.reactive.sec07;
 import com.antocecere77.reactive.courseutil.Util;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
+import reactor.util.concurrent.Queues;
 
 public class Lec02Drop {
 
     public static void main(String[] args) {
 
+        System.setProperty("reactor.bufferSize.small", "16");
+
         Flux.create(fluxSink -> {
             for (int i = 0; i < 501; i++) {
                 fluxSink.next(i);
                 System.out.println("Pushed: " + i);
+                Util.sleepMillis(1);
             }
             fluxSink.complete();
         }).onBackpressureDrop()
