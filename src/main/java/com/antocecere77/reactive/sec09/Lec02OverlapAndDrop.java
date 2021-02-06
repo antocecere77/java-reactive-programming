e47095ec92fc2f59dcd6d6a67302419323076a8f
antocecere <antocecere77@gmail.com>
2021-02-06 10:58:43 +0100

    Batching with buffer - overlapping & dropping

---
+++ src/main/java/com/antocecere77/reactive/sec09/Lec02OverlapAndDrop.java
@@ -0,0 +1,22 @@
+package com.antocecere77.reactive.sec09;
+
+import com.antocecere77.reactive.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lec02OverlapAndDrop {
+
+    public static void main(String[] args) {
+        eventStream()
+                .buffer(3, 1)
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(60);
+    }
+
+    private static Flux<String> eventStream() {
+        return Flux.interval(Duration.ofMillis(800))
+                .map(i -> "event" + i);
+    }
+}
