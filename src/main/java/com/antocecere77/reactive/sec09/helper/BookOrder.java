033d765afc3364373f062955700848723f8b994d
antocecere <antocecere77@gmail.com>
2021-02-08 06:32:40 +0100

    Book revenue by category stream

---
+++ src/main/java/com/antocecere77/reactive/sec09/helper/BookOrder.java
@@ -0,0 +1,25 @@
+package com.antocecere77.reactive.sec09.helper;
+
+import com.antocecere77.reactive.courseutil.Util;
+import com.github.javafaker.Book;
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class BookOrder {
+
+    private String title;
+    private String author;
+    private String category;
+    private double price;
+
+    public BookOrder() {
+        Book book = Util.faker().book();
+        this.title = book.title();
+        this.author = book.author();
+        this.category = book.genre();
+        this.price = Double.parseDouble(Util.faker().commerce().price().replaceAll(",", "."));
+    }
+}
