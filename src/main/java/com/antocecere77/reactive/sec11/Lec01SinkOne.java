17d8957bc7fd35ce7d242de72ff4117480bb541e
antocecere <antocecere77@gmail.com>
2021-02-09 07:51:46 +0100

    Sink one - multiple subscribers

--- src/main/java/com/antocecere77/reactive/sec11/Lec01SinkOne.java
+++ src/main/java/com/antocecere77/reactive/sec11/Lec01SinkOne.java
@@ -16,18 +16,20 @@ public class Lec01SinkOne {
 
         Mono<Object> mono = sink.asMono();
         mono.subscribe(Util.subscriber("sam"));
+        mono.subscribe(Util.subscriber("mike"));
 
-        sink.emitValue("hi", (signalType, emitResult) -> {
-            System.out.println(signalType.name());
-            System.out.println(emitResult.name());
-            return false;
-        });
+//        sink.emitValue("hi", (signalType, emitResult) -> {
+//            System.out.println(signalType.name());
+//            System.out.println(emitResult.name());
+//            return false;
+//        });
+//
+//        sink.emitValue("hi", (signalType, emitResult) -> {
+//            System.out.println(signalType.name());
+//            System.out.println(emitResult.name());
+//            return false;
+//        });
 
-        sink.emitValue("hi", (signalType, emitResult) -> {
-            System.out.println(signalType.name());
-            System.out.println(emitResult.name());
-            return false;
-        });
-
+        sink.tryEmitValue("Hello");
     }
 }
