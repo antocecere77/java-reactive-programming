a9504534e9d2ee53b8f531515dad610adff1dbcb
antocecere <antocecere77@gmail.com>
2021-02-11 07:41:04 +0100

    Step verifier - scenario name

---
+++ src/test/java/com/rp/test/Lex06ScenarioNameTest.java
@@ -0,0 +1,36 @@
+package com.rp.test;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.StepVerifierOptions;
+
+public class Lex06ScenarioNameTest {
+
+    @Test
+    public void test1() {
+
+        Flux<String> flux = Flux.just("a", "b", "c");
+
+        StepVerifierOptions scenarioName = StepVerifierOptions.create().scenarioName("alphabets-test");
+
+        StepVerifier.create(flux, scenarioName)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    public void test2() {
+
+        Flux<String> flux = Flux.just("a", "b", "c");
+
+        StepVerifier.create(flux)
+                .expectNext("a")
+                .as("a-test")
+                .expectNext("b")
+                .as("b-test")
+                .expectNext("c")
+                .as("c-test")
+                .verifyComplete();
+    }
+}
