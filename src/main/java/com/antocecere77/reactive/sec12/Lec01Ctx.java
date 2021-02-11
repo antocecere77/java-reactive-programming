052ef5fe3a72e2b4915e3cf994721f7f3d497f69
antocecere <antocecere77@gmail.com>
2021-02-11 06:34:30 +0100

    Context - demo - part 1

---
+++ src/main/java/com/antocecere77/reactive/sec12/Lec01Ctx.java
@@ -0,0 +1,24 @@
+package com.antocecere77.reactive.sec12;
+
+import com.antocecere77.reactive.courseutil.Util;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
+
+public class Lec01Ctx {
+
+    public static void main(String[] args) {
+        getWelcomeMessage()
+                .contextWrite(Context.of("user","sam"))
+                .subscribe(Util.subscriber());
+    }
+
+    private static Mono<String> getWelcomeMessage()  {
+        return Mono.deferContextual(ctx -> {
+            if(ctx.hasKey("user")) {
+                return Mono.just("Welcome " + ctx.get("user"));
+            } else {
+                return Mono.error(new RuntimeException("unauthenticated"));
+            }
+        });
+    }
+}
