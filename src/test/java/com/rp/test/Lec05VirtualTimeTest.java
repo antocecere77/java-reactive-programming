508a35c87dc32d9c0cc5b27d1e7900ef81ae9b4a
antocecere <antocecere77@gmail.com>
2021-02-11 07:31:25 +0100

    Step verifier - virtual time test - part 1

---
+++ src/test/java/com/rp/test/Lec05VirtualTimeTest.java
@@ -0,0 +1,28 @@
+package com.rp.test;
+
+import com.antocecere77.reactive.sec09.helper.BookOrder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class Lec05VirtualTimeTest {
+
+    @Test
+    public void test1() {
+
+        StepVerifier.withVirtualTime(() -> timeConsumingFlux())
+                .thenAwait(Duration.ofSeconds(30))
+                .expectNext("1a", "2a", "3a", "4a")
+                .verifyComplete();
+    }
+
+    private Flux<String> timeConsumingFlux() {
+        return Flux.range(1, 4)
+                .delayElements(Duration.ofSeconds(5))
+                .map(i -> i + "a");
+    }
+}
